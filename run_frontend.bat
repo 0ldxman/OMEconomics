dfabf9ab73a8901a92082e9ddd2b0a3ba105ad8d
0ldxman <0ldxman@users.noreply.github.com>
2026-06-08 08:31:24 +0300

    Tidy now

--- run_frontend.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd frontend/omeweb
-set NODE_OPTIONS=--max-old-space-size=4096
-set NEXT_TELEMETRY_DISABLED=1
-echo === ЗАПУСК ФРОНТЕНДА ===
-echo Дашборд будет доступен на http://localhost:3000
-npm run dev
-pause
